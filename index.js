28d84809f5020bff2ea67bdf0fa65e84a8084880
Vishal Bhardwaj <vishalbhardwaj820.vb@gmail.com>
2024-01-02 23:47:38 +0530

    WebSockets

---
+++ index.js
@@ -0,0 +1,28 @@
+const express=require("express");
+const app=express();
+const http=require("http");
+const server=http.createServer(app);
+const {Server}=require("socket.io");
+const cors=require("cors")
+app.use(cors())
+
+
+const io=new Server(server,{
+    cors:{
+        origin:"*"
+    }
+})
+ 
+io.on('connection',(socket)=>{
+    socket.on("send_message",(data)=>{
+        console.log("Message Recieved on server",data);
+        socket.broadcast.emit("recieve_message",data);
+    })
+    socket.on("disconnect",()=>{
+        console.log("user leave the chat ",socket.id);
+    })
+})
+
+server.listen(3000,()=>{
+    console.log(`Server Started at port 3000`);
+})
